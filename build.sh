c98f2bfdcc2850c9374400642a13cf8ee8f5c720
Luis mayta <luis.mayta@gmail.com>
2019-07-25 17:58:56 -0500

    limpiar build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 docker build -t jorarmarfin/drupal:tmp .
 docker build -t jorarmarfin/drupal:8.7.5 .
 
-docker run --name srv-drupal-apache -d jorarmarfin/drupal:8.7.5 
+docker run --name srv-drupal-apache -d jorarmarfin/drupal:8.7.5
